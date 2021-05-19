be738bd6f4b46bec989fb1c58776471471f9f886
Rhys Waters <info@rhyswaters.com>
2021-05-19 16:53:29 +0100

    .

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -51,6 +51,9 @@ export default function SignIn() {
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
+          <Typography>Kovidr</Typography>
+        </div>
+        <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
